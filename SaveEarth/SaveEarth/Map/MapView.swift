1785555931ca3dc5202be85da159dc2627bdee03
Hamin Song <cec7378@gmail.com>
2024-06-29 11:32:37 +0900

    MapView init

---
+++ SaveEarth/SaveEarth/Map/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  SaveEarth
+//
+//  Created by 송하민 on 6/29/24.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    MapView()
+}

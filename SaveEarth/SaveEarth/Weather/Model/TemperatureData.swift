f9fa1ecda1008a4a73a6e595a77ed3dd398d3ab2
Guboneui <starku2249@naver.com>
2024-06-29 16:16:15 +0900

    [FEAT] temperatureDeviation in TemperatureData

--- SaveEarth/SaveEarth/Weather/Model/TemperatureData.swift
+++ SaveEarth/SaveEarth/Weather/Model/TemperatureData.swift
@@ -18,4 +18,18 @@ struct TemperatureData {
     self.historicTemperature = round(historicTemperature * 10) / 10
     self.currentTemperature = round(currentTemperature * 10) / 10
   }
+  
+  /// 현재 온도와 평균온도 차이 계산
+  ///
+  /// - note: 현재 온도가 더 낮다면 0을 반환
+  func temperatureDeviation() -> Int {
+    let roundedHistoricTemperature: Int = Int(round(historicTemperature))
+    let roundedCurrentTemperature: Int = Int(round(currentTemperature))
+    
+    if roundedCurrentTemperature - roundedHistoricTemperature < 0 {
+      return 0
+    } else {
+      return roundedCurrentTemperature - roundedHistoricTemperature
+    }
+  }
 }

2d1b7e7a7d50ca019ea7ecca60a7d64f192a43cb
Guboneui <starku2249@naver.com>
2024-06-29 16:00:12 +0900

    [FEAT] WeatherManager

---
+++ SaveEarth/SaveEarth/Weather/WeatherManager.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherManager.swift
+//  SaveEarth
+//
+//  Created by 구본의 on 6/29/24.
+//
+
+import CoreLocation
+import Foundation
+import WeatherKit
+
+final class WeatherManager {
+  
+  private let weatherService: WeatherService
+  
+  init() {
+    self.weatherService = WeatherService()
+  }
+  
+  /// 평균 온도와, 현재 온도 반환
+  ///
+  /// - Parameters:
+  ///   - location: 날씨 검색 위치
+  func fetchHistoricalTemperature(
+    location: CLLocation
+  ) async throws -> TemperatureData? {
+    guard let mostSignificant = try await weatherService.weather(
+      for: location,
+      including: .historicalComparisons
+    )?.first
+    else {
+      return nil
+    }
+    switch mostSignificant {
+    case let .highTemperature(trend), let .lowTemperature(trend):
+      return TemperatureData(
+        historicTemperature: trend.baseline.value.value,
+        currentTemperature: trend.currentValue.value
+      )
+    default: break
+    }
+    return nil
+  }
+}

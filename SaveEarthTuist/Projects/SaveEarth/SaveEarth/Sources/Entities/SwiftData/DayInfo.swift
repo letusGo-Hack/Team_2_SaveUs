1af150b1f803895dced6db2c1dbaaccba9dfd20e
Guboneui <starku2249@naver.com>
2024-07-13 23:03:13 +0900

    [REFACTOR] `final` 키워드 적용

--- SaveEarthTuist/Projects/SaveEarth/SaveEarth/Sources/Entities/SwiftData/DayInfo.swift
+++ SaveEarthTuist/Projects/SaveEarth/SaveEarth/Sources/Entities/SwiftData/DayInfo.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-class DayInfo {
+final class DayInfo {
   #Unique<DayInfo>([\.date])
   let date: String
   let temperatureData: TemperatureSwiftDataModel

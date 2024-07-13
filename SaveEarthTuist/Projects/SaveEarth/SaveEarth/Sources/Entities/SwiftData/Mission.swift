4bd1d45308083bc68c239c2b249e989f55f29ca0
Guboneui <starku2249@naver.com>
2024-07-13 22:55:40 +0900

    [REFACTOR] Manager > Entities 로 폴더 명 변경

---
+++ SaveEarthTuist/Projects/SaveEarth/SaveEarth/Sources/Entities/SwiftData/Mission.swift
@@ -0,0 +1,27 @@
+//
+//  Mission.swift
+//  SaveEarth
+//
+//  Created by 김용우 on 7/6/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Mission {
+  #Unique<Mission>([\.id])
+  let id: UUID
+  let title: String
+  var isClear: Bool
+  
+  init(
+    id: UUID = UUID(),
+    title: String,
+    isClear: Bool = false
+  ) {
+    self.id = id
+    self.title = title
+    self.isClear = isClear
+  }
+}
---
+++ SaveEarthTuist/Projects/SaveEarth/SaveEarth/Sources/Entities/SwiftData/TemperatureSwiftDataModel.swift
@@ -0,0 +1,27 @@
+//
+//  TemperatureSwiftDataModel.swift
+//  SaveEarth
+//
+//  Created by 김용우 on 7/6/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class TemperatureSwiftDataModel {
+  #Unique<TemperatureSwiftDataModel>([\.id])
+  let id: UUID
+  let historicTemperature: Double
+  let currentTemperature: Double
+  
+  init(
+    id: UUID = UUID(),
+    historicTemperature: Double,
+    currentTemperature: Double
+  ) {
+    self.id = id
+    self.historicTemperature = historicTemperature
+    self.currentTemperature = currentTemperature
+  }
+}

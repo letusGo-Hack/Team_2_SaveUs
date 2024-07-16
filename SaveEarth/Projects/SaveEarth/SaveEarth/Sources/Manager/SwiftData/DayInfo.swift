6f12e73f3b55caea93aa705693628b105b64f505
Hamin Song <cec7378@gmail.com>
2024-07-16 22:08:51 +0900

    refactor: 기존 프로젝트 Tuist 프로젝트와 통합

---
+++ SaveEarth/Projects/SaveEarth/SaveEarth/Sources/Manager/SwiftData/DayInfo.swift
@@ -0,0 +1,28 @@
+//
+//  DayInfo.swift
+//  SaveEarth
+//
+//  Created by Importants on 6/29/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class DayInfo {
+    #Unique<DayInfo>([\.date])
+    let date: String
+    let temperatureData: TemperatureSwiftDataModel
+    var missionList: [Mission]
+    
+    
+    public init(
+        date: String,
+        temperatureData: TemperatureSwiftDataModel,
+        missionList: [Mission]
+    ) {
+        self.date = date
+        self.temperatureData = temperatureData
+        self.missionList = missionList
+    }
+}
